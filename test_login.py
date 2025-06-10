50a808b1e8df9aa0f47ca2310e22e91a77e013cb
vijaysayyaparaju <159777961+vijaysayyaparaju@users.noreply.github.com>
2025-06-10 15:04:44 +0530

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -2,7 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")  # Run headless
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")  # Make sure page is fully visible
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
 driver.get("https://www.saucedemo.com/")
 driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
